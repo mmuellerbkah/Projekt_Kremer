7da4971c47b5cecc20f1418f2c2dc7023eba1e02
morris.mueller <morris.mueller@ID042701>
2023-11-13 12:22:10 +0100

    Projektdateien hinzufügen.

---
+++ Projekt_Kremer/Data/MockDB.cs
@@ -0,0 +1,39 @@
+﻿namespace Projekt_Kremer.Data
+{
+    public class MockDB
+    {
+
+        public List<PlantCardDTO> Data { get; set; } = new List<PlantCardDTO>()
+        {
+            new PlantCardDTO()
+            {
+                Id =1,
+            Title = "Title 1",
+            Content = "Content 1",
+            ImageURL = "https://s3.amazonaws.com/images.ecwid.com/images/20892833/1240776484.jpg",
+                IsFavorite = true
+            },
+            new PlantCardDTO()
+            {
+                Id =2,
+            Title = "Title 2",
+            Content = "Content 2",
+            ImageURL = "https://s3.amazonaws.com/images.ecwid.com/images/20892833/1240776484.jpg"
+            },
+            new PlantCardDTO()
+            {
+                Id =3,
+            Title = "Title 3",
+            Content = "Content 3",
+            ImageURL = "https://s3.amazonaws.com/images.ecwid.com/images/20892833/1240776484.jpg"
+            },
+            new PlantCardDTO()
+            {
+                Id =4,
+            Title = "Title 4",
+            Content = "Content 4",
+            ImageURL = "https://s3.amazonaws.com/images.ecwid.com/images/20892833/1240776484.jpg"
+            }
+        };
+    }
+}
---
+++ Projekt_Kremer/Data/PlantCardDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Projekt_Kremer.Data
+{
+    public class PlantCardDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageURL { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
